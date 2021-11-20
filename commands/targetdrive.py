17b26c49628fa87a38a8a0d8403a70e18bc22cba
icai0 <icai2526@gmail.com>
2021-11-20 18:00:46 -0500

    turn limelight off when command ends

---
+++ commands/targetdrive.py
@@ -0,0 +1,24 @@
+from commands2 import CommandBase
+from subsystems.drivesubsystem import DriveSubsystem
+from networktables import NetworkTable
+
+class TargetDrive(CommandBase):
+    def __init__(self, drive: DriveSubsystem, table: NetworkTable) -> None:
+        CommandBase.__init__(self)
+
+        self.drive = drive
+        self.table = table
+
+        self.addRequirements([self.drive])
+        self.setName(__class__.__name__)
+
+    def execute(self) -> None:
+        if not self.table.getNumber('ty'):
+            #rotate in place slowly
+            pass
+        else:
+            #get distance of object from center (tx) rotate towards it
+            #move to object, scale speed based on how close objeect is (based on target size, ta)
+            pass
+    def end(self) -> None:
+        self.table.putNumber("ledMode", 1)
